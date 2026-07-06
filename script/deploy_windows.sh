a2faf4138da3883e5777306ea67bd7faa7a72e5c
R3G1ST <reg1st@proboy.dev>
2026-07-06 18:09:30 +0000

    fix: deploy updater.exe AFTER extracting artifacts
    artifacts.tgz was overwriting our new updater.exe with the old
    Throne one. Now extract artifacts first, then copy our updater.exe
    on top.

--- script/deploy_windows.sh
+++ script/deploy_windows.sh
@@ -4,12 +4,14 @@ set -e
 rm -rf $DEST
 mkdir -p $DEST
 
-#### copy exe ####
-cp $GITHUB_WORKSPACE/build/FreeLink.exe $DEST
-cp $GITHUB_WORKSPACE/build/FreeLink.pdb $DEST || true
-cp $GITHUB_WORKSPACE/updater/updater.exe $DEST || true
-
 cd download-artifact
 cd *$DEST_SUFFIX
 tar xvzf artifacts.tgz -C ../../
 cd ../..
+
+#### copy exe ####
+cp $GITHUB_WORKSPACE/build/FreeLink.exe $DEST
+cp $GITHUB_WORKSPACE/build/FreeLink.pdb $DEST || true
+# Copy updater LAST to avoid overwrite by artifacts.tgz
+cp $GITHUB_WORKSPACE/updater/updater.exe $DEST
+ls -la $DEST/updater.exe
